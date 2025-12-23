643e5fbc8d3d46a408ed4578a62b689e7dddadd4
이경도 <lkd9125@palnet.co.kr>
2025-12-23 18:34:49 +0900

    feat: user 기능 스펙정의

---
+++ src/main/java/com/geo/bridge/domain/user/UserService.java
@@ -0,0 +1,22 @@
+package com.geo.bridge.domain.user;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * <p>기능</p>
+ * 
+ * <ul>
+ *  <li>유저 호스트 검색</li>
+ *  <li>유저 데이터 포맷 검색</li>
+ *  <li>유저 생성</li>
+ * </ul>
+ */
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+
+}
---
+++ src/main/java/com/geo/bridge/domain/user/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * <p>유저 데이터 조작</p>
+ * 
+ * <ul>
+ *  <li>호스트 저장</li>
+ *  <li>데이터 포맷팅 저장</li>
+ * </ul>
+ */
+package com.geo.bridge.domain.user;
